073c17010c31ba3837994c922960cf7872aa9fe6
Noureldin247 <145559888+Noureldin247@users.noreply.github.com>
2023-11-30 15:40:17 +0200

    added the encryption and decryption

--- FINALCODE.py
+++ FINALCODE.py
@@ -8,3 +8,9 @@ Dictionary= {
     ' ': ' '
 }
 
+def encrypt_to_morse(phrase):
+    return ' '.join(Dictionary.get(char.upper(), ' ') for char in phrase)
+
+def decrypt_from_morse(morse_code):
+    morse_code = morse_code.split(' ')
+    return ''.join(key for code in morse_code for key, value in Dictionary.items() if code == value)
