4aed51abbd45f045568172bb1873f57312d7b400
Noureldin247 <145559888+Noureldin247@users.noreply.github.com>
2023-11-30 19:48:09 +0200

    finished the code and tested

--- FINALCODE.py
+++ FINALCODE.py
@@ -16,3 +16,29 @@ def decrypt_from_morse(morse_code):
     return ''.join(key for code in morse_code for key, value in Dictionary.items() if code == value)
 
 def start_app():
+    print("WELCOME TO THE MORSE CODE TOOL")
+    while True:
+        print("Select an action:")
+        print("'!' for Encryption")
+        print("'$' for Decryption")
+        print("'#' to Exit")
+
+        user_choice = input().upper()
+
+        if user_choice == '!':
+            plaintext = input("Enter the text to encrypt: ")
+            encrypted_text = encrypt_to_morse(plaintext)
+            print(f"Encrypted text: {encrypted_text}\n")
+        elif user_choice == '$':
+            morse_code = input("Enter Morse code to decrypt: ")
+            decrypted_text = decrypt_from_morse(morse_code)
+            print(f"Decrypted text: {decrypted_text}\n")
+        elif user_choice == '#':
+            print("Thank you for using this application, goodbye.")
+            break
+        else:
+            print("Unknown option. Please select '!' for Encryption, '$' for Decryption, or '#' to Exit.\n")
+
+
+if __name__ == "__main__":
+    start_app()
